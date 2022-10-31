60276d5a4b2a023e33c241c5a6a771be2300fb0e
prosenjit005 <117080260+MyItWorldPro@users.noreply.github.com>
2022-10-31 16:23:12 +0530

    initial commit

---
+++ easy1/src/main/java/com/myit/easy1/Easy1Application.java
@@ -0,0 +1,13 @@
+package com.myit.easy1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Easy1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Easy1Application.class, args);
+	}
+
+}
